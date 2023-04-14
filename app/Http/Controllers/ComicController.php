dbed00873a56f6a45c44e9e70785f77e874d693f
nicola00 <nicolajobs00@gmail.com>
2023-04-14 16:20:04 +0200

    CREATE function index in controlle and route in web

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    //
+    public function index()
+    {
+        $c = Comic::all();
+
+        return view('comics.index', compact('c'));
+    }
 }
