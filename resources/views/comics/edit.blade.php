2b47a002a7fe9c18eeb935bfc84fa48ba0ac2aa7
nicola00 <nicolajobs00@gmail.com>
2023-04-17 16:56:47 +0200

    FEAT form in edit page and add value in input

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -2,6 +2,58 @@
 
 @section('content')
 
-<h1>Modifica i personaggi</h1>
+<div class="container">
+    <h1>Modifica il personaggio:
+        {{ $comic->title }}
+    </h1>
+
+</div>
+
+<div class="container">
+    <form action="" method="POST">
+
+        @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="price" class="form-label">prezzo</label>
+            <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="type" class="form-label">Tipologia</label>
+            <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="sale_date" class="form-label">data di vendita</label>
+            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="series" class="form-label">series</label>
+            <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="thumb" class="form-label">image</label>
+            <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">description</label>
+            <textarea class="form-control" name="description" id="description" cols="30" rows="10">
+            {{ $comic->description }}
+            </textarea>
+        </div>
+
+        <button class="btn btn-success my-4">Salva</button>
+
+    </form>
+</div>
 
 @endsection
