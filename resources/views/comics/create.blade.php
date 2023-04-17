98b557c779875b302d54fe022b1118d987c18c85
nicola00 <nicolajobs00@gmail.com>
2023-04-17 15:10:34 +0200

    FEAT form in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <h1>crea un nuovo personaggio</h1>
 
-<h1>crea un nuovo personaggio</h1>
+</div>
+
+<div class="container">
+    <form action="">
+
+
+
+    </form>
+</div>
 
 @endsection
